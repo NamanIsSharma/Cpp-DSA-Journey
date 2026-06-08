3a70d17c370c7c76ba01d4fee1b650acd507d065
Naman Sharma <namansharmajnv@gmail.com>
2026-06-08 09:56:33 +0530

    Update pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -14,13 +14,46 @@ int main(){
 }*/
 
 ///////// square pattern (*)//////
-#include<iostream>
+/* #include<iostream>
 using namespace std;
 int main(){
     int n = 4;
     for(int i=0; i<=n-1; i++){  //outer loop
         for(int j=1; j<=n; j++){  //inner loop
             cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}*/
+
+///////// square pattern (ABCD)//////
+/* #include<iostream>
+using namespace std;
+int main(){
+    int n = 4;
+    for(int i=0; i<=(n-1); i++){  //outer loop
+        char ch = 'A';
+        for(int j=0; j<=(n-1); j++){  //inner loop
+            cout<<ch<<" ";
+            ch++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}*/
+
+////////////// Square pattern (123-456-789)//////////
+#include<iostream>
+using namespace std;
+int main(){
+    int n, num=1;
+    cout<<"Enter n: ";
+    cin>>n;
+    for(int i=0; i<=(n-1); i++){
+        for(int j=0; j<=(n-1); j++){
+            cout<<num<<" ";
+            num++;
         }
         cout<<endl;
     }
