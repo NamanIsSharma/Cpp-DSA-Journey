c1558bba2412d81b38777c3f8cf6245e267603fe
Naman Sharma <namansharmajnv@gmail.com>
2026-06-13 14:47:35 +0530

    Update only function.cpp file

--- function.cpp
+++ function.cpp
@@ -18,15 +18,44 @@ int main(){
     return 0;
 }*/
 
-#include<iostream>
+/* #include<iostream>
 using namespace std;
 int minOfTwoNo(int a, int b){  // parameters
     if(a<b){
-        return a;
+        return a;    // MinOfTwo //
     } else{
         return b;
     }
 }
 int main(){
     cout<< "min = " << minOfTwoNo(5,3);  //arguments
+}*/
+
+/* #include<iostream>
+using namespace std;
+int Sum_Of_N(int n){
+    int sum = 0;
+    for(int i=1; i<=n; i++){    // sum of n no.
+        sum+=i;
+    }
+    return sum;
+}
+int main(){
+    cout<<Sum_Of_N(6);
+    return 0;
+}*/
+
+
+#include<iostream>
+using namespace std;
+int FactN(int n){
+    int Fact = 1;
+    for(int i=1; i<=n; i++){
+        Fact = Fact*i;
+    }
+    return Fact;
+}
+int main(){
+    cout<<"Factorial = "<<FactN(7);
+    return 0;
 }
