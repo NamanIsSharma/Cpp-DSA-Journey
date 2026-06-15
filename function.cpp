4322535807ac0749ef35ac381d2df7aeb396b144
Naman Sharma <namansharmajnv@gmail.com>
2026-06-15 18:33:30 +0530

    Update @ 15-06 till 6pm

--- function.cpp
+++ function.cpp
@@ -76,13 +76,13 @@ int main(){
     return 0;
 }*/
 
-#include<iostream>
+/* #include<iostream>
 using namespace std;
 int SumOfDigits(int num){
     int DigitSum = 0;
 
     while(num>0){
-        int lastDigit = num % 10;
+        int lastDigit = num % 10;         // Sum of Digits
         num = num/10;
 
         DigitSum += lastDigit;
@@ -91,5 +91,57 @@ int SumOfDigits(int num){
 }
 int main(){
     cout<< "Sum of digits = "<< SumOfDigits(12345);
+    return 0;
+}*/
+
+/////////// nCr //////////
+/*#include<iostream>
+using namespace std;
+
+int factorial(int n){
+    int fact = 1;
+    for(int i = 1; i<=n; i++){
+        fact *= i;
+    }
+    return fact;
+}
+
+int nCr(int n, int r){
+    int fact_n = factorial(n);
+    int fact_r = factorial(r);
+    int fact_nmr = factorial(n-r);
+
+    return fact_n/(fact_r * fact_nmr);
+}
+
+int main(){
+    int n = 8, r = 2;
+    cout<< nCr(n,r) << endl;
+
+    return 0;
+}*/
+
+#include<iostream>
+using namespace std;
+
+bool isPrime(int n){
+    
+    for(int i=2; i<=(n-1); i++){
+        if(n%i==0)
+        return false;
+    }
+    return true;
+    
+}
+int main(){
+    int n;
+    cin>>n;
+
+    if(isPrime(n)){
+        cout << "Prime No.";
+    }
+    else{
+        cout<< "Non-Prime";
+    }
     return 0;
 }
