af32a3818f337b6e8f13c455a1847f965e0e18ac
Naman Sharma <namansharmajnv@gmail.com>
2026-06-16 10:15:24 +0530

    Final Update

--- binNoSys.cpp
+++ binNoSys.cpp
@@ -1,5 +1,5 @@
 ////// Decimal to Binary ////
-#include<iostream>
+/* #include<iostream>
 using namespace std;
 
 int DecToBin(int decNum){
@@ -20,4 +20,27 @@ int main(){
     // for(int i=0; i<=10; i++)  //dectobin(1 to 10)
 
     cout<< DecToBin(decNum)<<endl;
+}*/ 
+
+
+/////  Binary to Decimal  ////
+#include<iostream>
+using namespace std;
+
+int BinToDec(int binNum){
+    int ans = 0, pow = 1;
+    while(binNum > 0){
+        int rem = binNum % 2;
+        ans += (rem*pow);
+
+        binNum /= 10;
+        pow *= 2;
+    }
+    return ans;
+}
+int main(){
+    int binNum = 110011;
+    
+    cout << BinToDec(binNum) << endl;
+    return 0;
 }
