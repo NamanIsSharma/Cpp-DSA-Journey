64bb44c72fdd8c9673005829d7cac33a98d6d580
Naman Sharma <namansharmajnv@gmail.com>
2026-06-27 12:17:35 +0530

    .

--- Array.cpp
+++ Array.cpp
@@ -152,7 +152,7 @@ int main() {
 
 
 ///////////  Reverse of an Array  //////////
-#include<iostream>
+/* #include<iostream>
 using namespace std;
 
 int RevArray(int arr[], int size){
@@ -173,5 +173,63 @@ int main(){
     for(int i=0; i<size; i++){
         cout << arr[i] << " ";
     }
+    return 0;
+}?*/
+
+////////// sum of elements of an array  ///////
+/* #include <iostream>
+using namespace std;
+int main() {
+    int arr[] = {5,4,1,3,2};
+    int size = 5;
+    int sum = 0;
+    for(int i=0; i<size; i++){
+        sum = sum + arr[i]; // sum+=arr[i]
+    }
+    cout << sum; 
+    return 0;
+}
+
+//////////// sum using function  ////////////
+#include <iostream>
+using namespace std;
+int Sum_No_Array(int arr[], int size){
+    int sum = 0;
+    for(int i=0; i<size; i++){
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int main() {
+    int size = 6;
+    int arr[size];
+    for(int i=0;  i<size; i++){
+        cin >> arr[i];
+    }
+
+    cout << Sum_No_Array(arr, size);
+    
+    return 0;
+}*/
+
+
+//////////// product of elements in an array  ////////
+#include <iostream>
+using namespace std;
+
+Mul_No_Array(int arr[], int size){
+    int product = 1;
+    for(int i=0; i<size; i++){
+        product *= arr[i];
+    }
+    return product;
+}
+
+int main() {
+    int arr[] = {1,2,3,4,5};
+    int size = 5;
+
+    cout << Mul_No_Array(arr, size);
     return 0;
 }
