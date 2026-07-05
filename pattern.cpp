3c490a23d649c689467b5c847831cca2d0b25add
Naman Sharma <namansharmajnv@gmail.com>
2026-07-05 11:29:17 +0530

    Update in pattern

--- pattern.cpp
+++ pattern.cpp
@@ -203,7 +203,7 @@ int main(){
 }*/
 
 //////// Pyramid Pattern (1-121-12321-1234321)//
-#include<iostream>
+/* #include<iostream>
 using namespace std;
 int main(){
     int n;
@@ -222,5 +222,37 @@ int main(){
         }
         cout<<endl;
     }
+    return 0;
+}*/
+
+//////// revision /////////
+
+#include <iostream>
+using namespace std;
+void pattern1(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern2(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<=i; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+
+int main() {
+    int n;
+    cout << "Enter n : ";
+    cin >> n;
+    // pattern1(n);   // square of *
+    pattern2(n);
     return 0;
 }
