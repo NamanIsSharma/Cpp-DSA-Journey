a136c1b0fa32a3f63f0b1250b53682509831e817
Naman Sharma <namansharmajnv@gmail.com>
2026-08-05 23:11:53 +0530

    Update in array.striver

--- Array_Striver.cpp
+++ Array_Striver.cpp
@@ -18,7 +18,7 @@ int main() {
 
 ///////// Remove dublicates elements from an array
 
-#include <iostream>
+/* #include <iostream>
 using namespace std;
 int remove_dublicates(int arr[],int n){
     int i=0;
@@ -42,5 +42,31 @@ int main() {
     for(int i=0; i<k; i++){ // k tak iterate karenge
         cout << arr[i] << " ";
     }
+    return 0;
+}*/
+
+
+//////// print second largest no from array (optimal approach with T.C = O(N))
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 6;
+    int arr[] = {1,3,4,4,7,5};
+
+    int largest = arr[0];
+    int s_largest = -1;
+
+    for(int i=0; i<n; i++){
+        if(arr[i] > largest){
+            s_largest = largest;
+            largest = arr[i];
+        }
+        else if(arr[i] < largest && arr[i] > s_largest){
+            s_largest = arr[i];
+        }
+    }
+    cout << s_largest;
     return 0;
 }
