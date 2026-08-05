ecb23b446bf64402fb5b0d7809794a5b3faaa6f6
Naman Sharma <namansharmajnv@gmail.com>
2026-08-05 16:14:13 +0530

    Array using striver

--- Array_Striver.cpp
+++ Array_Striver.cpp
@@ -1,6 +1,6 @@
 ///////// Print largest number of an array
 
-#include <iostream>
+/* #include <iostream>
 using namespace std;
 
 int main() {
@@ -12,5 +12,35 @@ int main() {
         largest = arr[i];
     }
     cout << largest;
+    return 0;
+}*/
+
+
+///////// Remove dublicates elements from an array
+
+#include <iostream>
+using namespace std;
+int remove_dublicates(int arr[],int n){
+    int i=0;
+    for(int j=0; j<n; j++){
+        if(arr[i] != arr[j]){
+            arr[i+1] = arr[j];
+            i++;
+        }
+    }
+    return i+1;
+}
+
+int main() {
+    int n = 5;
+    int arr[] = {1,1,2,2,3};
+
+    int k = remove_dublicates(arr,n); //k mein store ho gyi i+1 ki value
+
+    cout << k << endl; // k is now new array size
+
+    for(int i=0; i<k; i++){ // k tak iterate karenge
+        cout << arr[i] << " ";
+    }
     return 0;
 }
